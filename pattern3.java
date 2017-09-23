7b4ada7653366c96aeea2eadd158ad1b104fdd45
Tarun Mahajan <tarunmahajan4567@hotmail.com>
2017-09-23 15:30:05 +0530

    deleted
    back up

--- pattern3.java
+++
@@ -1,9 +0,0 @@
-public class pattern3
-{
-public static void main(String ar[])
-{int n=5,i,j,x;
-	
-
-}
-
-}
--- pris.java
+++
@@ -1,9 +0,0 @@
-public class pris{
-public static void main(String args[]){
-int i=1,j=0;
-j=++i;
-System.out.println("j is = "+j+"and i ="+i);
-j=i++;
-System.out.println("j is = "+j+"and i ="+i);
-}  
-}
